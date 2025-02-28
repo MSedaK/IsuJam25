7e6e4dfcb85c4d84b39e0466e9457f2055989e3f
Melike Seda Ketenci <115991225+MSedaK@users.noreply.github.com>
2025-02-28 21:51:29 +0300

    dogru kombinasyon yanlista durma sekli

--- Assets/CombinationInput.cs
+++ Assets/CombinationInput.cs
@@ -1,58 +1,80 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class CombinationInput : MonoBehaviour
 {
-    public Text comboText;  
-    public float comboTime = 4f;  
-    private string correctCombo = "WSDA";  
+    public Text comboText;  // UI'da kombinasyon göstermek için
+    public float comboTime = 2f;  // Her bir tuþ için verilen süre
+    private string correctCombo = "WASD";  // Doðru kombinasyon
     private float timer;
-    private string currentInput = "";
+    private int currentComboIndex = 0;  // Sýradaki beklenen tuþun indexi
 
-    private Animator animator; 
-    public int health = 100;  
+    private Animator animator;  // Karakterin Animator bileþeni
+    public int health = 100;  // Oyuncunun saðlýðý
+    private bool isComboActive = false;  // Kombinasyon baþladý mý?
 
     void Start()
     {
         animator = GetComponent<Animator>();
         timer = comboTime;
-        comboText.text = "SHAKE IT UP OR...";  
+        comboText.text = "SHAKE IT UP OR...";  // Ýlk mesaj
     }
 
     void Update()
     {
-        timer -= Time.deltaTime;
-
-        if (timer <= 0)
+        // Eðer kombinasyon aktifse, zaman sayacý iþler
+        if (isComboActive)
         {
-            comboText.text = "TIME'S UP!";  
-            HealthChange(-10);  
-            ResetCombo();
+            timer -= Time.deltaTime;
+
+            // Eðer süre biterse, Victory animasyonunu anýnda kes ve Idle'a dön
+            if (timer <= 0)
+            {
+                comboText.text = "TIME'S UP!";
+                HealthChange(-10);  // Saðlýk azalt
+                ResetToIdle();
+            }
         }
 
-        if (Input.GetKeyDown(KeyCode.W)) AddInput("W");
-        if (Input.GetKeyDown(KeyCode.S)) AddInput("S");
-        if (Input.GetKeyDown(KeyCode.D)) AddInput("D");
-        if (Input.GetKeyDown(KeyCode.A)) AddInput("A");
+        // Tuþ giriþlerini kontrol et
+        if (currentComboIndex < correctCombo.Length)
+        {
+            if (Input.GetKeyDown(KeyCode.W)) CheckInput("W");
+            if (Input.GetKeyDown(KeyCode.A)) CheckInput("A");
+            if (Input.GetKeyDown(KeyCode.S)) CheckInput("S");
+            if (Input.GetKeyDown(KeyCode.D)) CheckInput("D");
+        }
     }
 
-    void AddInput(string key)
+    void CheckInput(string key)
     {
-        currentInput += key;
-        if (currentInput == correctCombo)
+        // Eðer kombinasyon daha önce baþlamadýysa ve ilk tuþa basýldýysa, Victory animasyonu baþlat
+        if (!isComboActive && key == correctCombo[0].ToString())
         {
-            comboText.text = "SUCCESS!";
-            HealthChange(10); 
-            animator.SetTrigger("Victory"); 
-            ResetCombo();
+            animator.Play("Victory", 0, 0f);  // Victory animasyonunu baþlat (SIFIRDAN)
+            isComboActive = true;
         }
-        else if (!correctCombo.StartsWith(currentInput))
+
+        // Eðer doðru tuþa bastýysa
+        if (key == correctCombo[currentComboIndex].ToString())
+        {
+            currentComboIndex++;
+            timer = comboTime;  // Süreyi yenile
+            comboText.text = "Success: " + currentComboIndex + "/" + correctCombo.Length;
+
+            // Kombinasyon tamamlandýðýnda saðlýk kazan
+            if (currentComboIndex == correctCombo.Length)
+            {
+                comboText.text = "COMBO SUCCESS!";
+                HealthChange(10);  // Saðlýk artýr
+                ResetToIdle();  // Victory bitti, tekrar Idle'a dön
+            }
+        }
+        else  // Yanlýþ tuþ basýldýysa Victory anýnda durmalý ve Idle'a geçmeli
         {
             comboText.text = "WRONG COMBO!";
-            HealthChange(-10);  
-            ResetCombo();
+            HealthChange(-10);  // Saðlýk azalt
+            ResetToIdle();  // Victory iptal, anýnda Idle'a geç
         }
     }
 
@@ -62,9 +84,11 @@ public class CombinationInput : MonoBehaviour
         comboText.text += "\nHealth: " + health;
     }
 
-    void ResetCombo()
+    void ResetToIdle()
     {
-        currentInput = "";
-        timer = comboTime;  
+        currentComboIndex = 0;  // Kombinasyonu sýfýrla
+        timer = comboTime;  // Zamaný sýfýrla
+        isComboActive = false;  // Kombinasyonu sýfýrla
+        animator.Play("Idle", 0, 0f);  // ANÝMASYONU ANINDA IDLE'A GEÇÝR
     }
 }

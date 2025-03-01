8d25ede3a96bd89361962f82a375535e3cd8ca12
Melike Seda Ketenci <115991225+MSedaK@users.noreply.github.com>
2025-03-02 01:23:19 +0300

    pushhh

--- Assets/Scripts/Stone.cs
+++ Assets/Scripts/Stone.cs
@@ -4,22 +4,25 @@ using UnityEngine;
 
 public class Stone : MonoBehaviour
 {
-    public int damage = 10; 
-    public string targetTag; 
+    public int damage = 10;
+    public string targetTag;
 
     void Start()
     {
+        Destroy(gameObject, 3f); 
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(targetTag)) 
+        if (other.CompareTag(targetTag))
         {
             PlayerHealth playerHealth = other.GetComponent<PlayerHealth>();
             if (playerHealth != null)
             {
                 playerHealth.TakeDamage(damage);
             }
+
+            Destroy(gameObject); 
         }
     }
 }
